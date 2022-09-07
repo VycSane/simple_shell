d364456e18903f4d136d2ae432f2feff19ae0e73
Jehiel-MW <olami.solomon@gmail.com>
2022-09-07 09:44:49 -0700

    the string file and its header file

---
+++ _strings.h
@@ -0,0 +1,9 @@
+#ifndef _STRINGS_H
+#define _STRINGS_H
+#include <stdlib.h>
+#include <stdio.h>
+size_t _strlen(char *str);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+#endif /* _STRINGS_H */
