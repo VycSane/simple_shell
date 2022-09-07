ac8ad19d2b8bcef518550022dda1b59a1a7296d9
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-07 15:52:07 -0700

    Added the _strcmp prototype to _strings.h header file

--- _strings.h
+++ _strings.h
@@ -6,4 +6,5 @@ size_t _strlen(char *str);
 char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 #endif /* _STRINGS_H */
